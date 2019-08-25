bd7832f51a2671d19f990d15ff365d15b9cf403d
Damir Bajramovic <bajramovic.damir@hotmail.com>
2019-08-25 00:51:54 -0700

    Initial commit

---
+++ src/main/java/com/red5pro/example/ExampleApplication.java
@@ -0,0 +1,90 @@
+package com.red5pro.example;
+
+import org.red5.net.websocket.WSConstants;
+import org.red5.net.websocket.WebSocketPlugin;
+import org.red5.net.websocket.WebSocketScope;
+import org.red5.net.websocket.WebSocketScopeManager;
+import org.red5.server.adapter.MultiThreadedApplicationAdapter;
+import org.red5.server.api.IConnection;
+import org.red5.server.api.scope.IScope;
+import org.red5.server.plugin.PluginRegistry;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class ExampleApplication extends MultiThreadedApplicationAdapter implements ApplicationContextAware{
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public boolean appConnect(IConnection conn, Object[] params) {
+      log.info("appConnect");
+      return super.appConnect(conn, params);
+    }
+
+    @Override
+    public void appDisconnect(IConnection conn) {
+      log.info("appDisconnect");
+      super.appDisconnect(conn);
+    }
+
+    @Override
+    public boolean appStart(IScope app) {
+      log.info("appStart");
+
+      configureApplicationScopeWebSocket(scope);
+
+      return super.appStart(app);
+    }
+
+    @Override
+    public void appStop(IScope scope) {
+
+      WebSocketScopeManager manager = ((WebSocketPlugin) PluginRegistry.getPlugin("WebSocketPlugin")).getManager(scope);
+        manager.removeApplication(scope);
+        manager.stop();
+
+    super.appStop(scope);
+    }
+
+    /**
+     * Configures a websocket scope for a given application scope.
+     *
+     * @param scope Server application scope
+     */
+    private void configureApplicationScopeWebSocket(IScope scope) {
+
+        // first get the websocket plugin
+        WebSocketPlugin wsPlugin = ((WebSocketPlugin) PluginRegistry.getPlugin(WebSocketPlugin.NAME));
+        // get the websocket scope manager for the red5 scope
+        WebSocketScopeManager manager = wsPlugin.getManager(scope);
+
+        if (manager == null) {
+            // get the application adapter
+            MultiThreadedApplicationAdapter app = (MultiThreadedApplicationAdapter) scope.getHandler();
+            log.debug("Creating WebSocketScopeManager for {}", app);
+            // set the application in the plugin to create a websocket scope manager for it
+            wsPlugin.setApplication(app);
+            // get the new manager
+            manager = wsPlugin.getManager(scope);
+        }
+
+        // the websocket scope
+        WebSocketScope wsScope = (WebSocketScope) scope.getAttribute(WSConstants.WS_SCOPE);
+
+        // check to see if its already configured
+        if (wsScope == null) {
+            log.debug("Configuring application scope: {}", scope);
+            // create a websocket scope for the application
+            wsScope = new WebSocketScope(scope);
+            // register the ws scope
+            wsScope.register();
+        }
+    }
+
+  }
---
+++ src/main/webapp/readme.md
@@ -0,0 +1,7 @@
+# WEB-RTC running examples
+
+These examples are to be used with a running Red5Pro server. 
+Currently, they are set up to be used in the localhost, without TSL. 
+In the future we will set them up for the TSL and they will be located in a different repository.
+OR a different branch. 
+Once we get ready to deploy on the production servers, we will start setting up TSL.
